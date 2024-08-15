f8d9eb70443fd778f544591fc4258dba4d00c0be
lishiyun <2696955741@qq.com>
2024-08-15 08:27:31 +0800

    Delete js directory
    Signed-off-by: lishiyun <2696955741@qq.com>

--- js/jianzhu.js
+++
@@ -1,7 +0,0 @@
-function showSection(section) {
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(sec => {
-        sec.classList.remove('active');
-    });
-    document.getElementById(section).classList.add('active');
-}
